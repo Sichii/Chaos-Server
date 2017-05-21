b7c19cfeb3eacc96b2f4f688c0ef70605b5e7247
zahdjinn <despereaux@live.com>
2017-05-21 04:13:39 -0500

    small change

--- Insert Creative Name/Client/Panel.cs
+++ Insert Creative Name/Client/Panel.cs
@@ -20,8 +20,10 @@ namespace Insert_Creative_Name
 
         internal Panel(byte length)
         {
-            Objects = new SortedDictionary<byte, T>();
             Length = length;
+            Objects = new SortedDictionary<byte, T>();
+            for (byte i = 0; i < length; i++)
+                Objects[i] = null;
 
             if(length == 90) //skillbook and spellbook
                 Invalid = new byte[] { 36, 72, 89 };
@@ -39,25 +41,31 @@ namespace Insert_Creative_Name
                 Objects[obj.Slot] = obj;
         }
 
-        //attempts to remove the object at the specified slot. returns false if invalid slot/doesnt exist/fails to remove
-        internal bool TryRemove(byte slot) => Valid(slot) && Objects.ContainsKey(slot) && Objects.Remove(slot);
+        //attempted set the value of the slot to null, otherwise return false
+        internal bool TryRemove(byte slot)
+        {
+            if (Valid(slot))
+            {
+                Objects[slot] = null;
+                return true;
+            }
+            return false;
+        }
 
         //attempts to remove the object at the specified slot and return it. returns false if invalid slot/doesnt exist/fails to remove
         internal bool TryGetRemove(byte slot, out T obj)
         {
-            if (!Valid(slot) || !Objects.ContainsKey(slot))
-                return (obj = null) != null;
-
             obj = Objects[slot];
-            return Objects.Remove(slot);
+            if (TryRemove(slot))
+                return true;
+
+            obj = null;
+            return false;
         }
 
         //attempts to swap two objects. returns false if it fails to remove either object for any reason.
         internal bool TrySwap(byte slot1, byte slot2)
         {
-            if (!Valid(slot1) || !Valid(slot2))
-                return false;
-
             T one, two;
 
             if (TryGetRemove(slot1, out one) && TryGetRemove(slot2, out two))
@@ -66,7 +74,7 @@ namespace Insert_Creative_Name
                 two.Slot = slot1;
                 Add(one);
                 Add(two);
-
+                
                 return true;
             } //puts the first object back if it succeeded on the first operation, but failed on the second
             else if (one != null)
