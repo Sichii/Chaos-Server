b9be720e8bf470a9624af02c486dfc5d3a8a86c2
Brandon Acton <despereaux@live.com>
2022-05-12 22:34:24 -0400

    data files

---
+++ Tools/Benchmarks/LockBenchmarks.cs
@@ -0,0 +1,36 @@
+using BenchmarkDotNet.Attributes;
+using Chaos.Core.Utilities;
+
+namespace Benchmarks;
+
+[MemoryDiagnoser]
+public class LockBenchmarks
+{
+    private readonly object Sync = new object();
+    private readonly AutoReleasingMonitor Auto = new AutoReleasingMonitor();
+    
+    [Params(100, 1000, 10000)]
+    public int NumRuns;
+
+    [Benchmark(Baseline = true)]
+    public void NormalLock()
+    {
+        var random = Random.Shared.Next(0, 20);
+        
+        for (var i = 0; i < NumRuns; i++)
+            lock (Sync)
+                random += Random.Shared.Next(0, 20);
+    }
+
+    [Benchmark]
+    public void AutoLock()
+    {
+        var random = Random.Shared.Next(0, 20);
+        
+        for (var i = 0; i < NumRuns; i++)
+        {
+            using var @lock = Auto.Enter();
+            random += Random.Shared.Next(0, 20);
+        }
+    }
+}
