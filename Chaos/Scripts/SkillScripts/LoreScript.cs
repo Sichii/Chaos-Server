5a047fa07edc0aca045fc1ed075b67695cf9e82b
Michael Bivins <mike.bivins@gmail.com>
2022-11-05 11:13:02 -0400

    Lore Skill Script
    *New generic skill that checks first slot for certain information. Highly configurable.

---
+++ Chaos/Scripts/SkillScripts/LoreScript.cs
@@ -0,0 +1,31 @@
+﻿using Chaos.Common.Definitions;
+using Chaos.Objects.Panel;
+using Chaos.Objects.World.Abstractions;
+using Chaos.Objects.World;
+using Chaos.Scripts.SkillScripts.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaos.Scripts.SkillScripts
+{
+    public class LoreScript : SkillScriptBase
+    {
+        public LoreScript(Skill subject) : base(subject)
+        {
+        }
+
+        public override void OnUse(Creature source)
+        {
+            foreach (var aisling in source.MapInstance.GetEntitiesWithinRange<Aisling>(source))
+            {
+                if (aisling.Id == source.Id)
+                {
+                    aisling.Client.SendServerMessage(ServerMessageType.WoodenBoard, "Name: " + aisling.Inventory[1]?.DisplayName + "\nValue: " + aisling.Inventory[1]?.Template.Value + "\nArmor Class: " + aisling.Inventory[1]?.Template?.Modifiers?.Ac);
+                }
+            }
+        }
+    }
+}
