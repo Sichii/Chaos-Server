2850a1f8d744480fb1f05a40dbec6d3f66599099
Michael Bivins <mike.bivins@gmail.com>
2022-11-05 10:54:47 -0400

    Look skill
    *New skill displaying current map name and height/width. This can also be a base for a rogue skill that will allow you to see map spawns possibly.

---
+++ Chaos/Scripts/SkillScripts/LookScript.cs
@@ -0,0 +1,32 @@
+﻿using Chaos.Common.Definitions;
+using Chaos.Containers;
+using Chaos.Objects.Panel;
+using Chaos.Objects.World;
+using Chaos.Objects.World.Abstractions;
+using Chaos.Scripts.SkillScripts.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaos.Scripts.SkillScripts
+{
+    public class LookScript : SkillScriptBase
+    {
+        public LookScript(Skill subject) : base(subject)
+        {
+        }
+
+        public override void OnUse(Creature source)
+        {
+            foreach (var aisling in source.MapInstance.GetEntitiesWithinRange<Aisling>(source))
+            {
+                if (aisling.Id == source.Id)
+                {
+                    aisling.Client.SendServerMessage(ServerMessageType.WoodenBoard, "Map Name: " + aisling.MapInstance.Name + " \nHeight: " + aisling.MapInstance.Template.Height + " \nWidth: " + aisling.MapInstance.Template.Width);
+                }
+            }
+        }
+    }
+}
