43b731bdce4d0f1614df46283e94b4a3ab385618
sichii <despereaux@live.com>
2024-11-17 18:05:53 -0500

    release bat
    fix pack.yml
    
    release bat update
    
    asdf
    
    asdff
    
    fdsa

--- Tools/do-release.bat
+++ Tools/do-release.bat
@@ -1,1 +1,12 @@
+SET /P Version="Next Version: v"
+
+git fetch origin
 nbgv prepare-release
+git branch -m v%Version% release/v%Version%
+git push origin release/v%Version%
+git push
+git checkout master
+git merge release/v%Version%
+git push
+git checkout develop
+pause
