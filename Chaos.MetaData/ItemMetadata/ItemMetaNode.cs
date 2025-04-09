80332c3c277bed7dfbae7df452f8332896271419
sichii <despereaux@live.com>
2025-04-09 09:32:12 -0400

    sped up item metadata generation

--- Chaos.MetaData/ItemMetadata/ItemMetaNode.cs
+++ Chaos.MetaData/ItemMetadata/ItemMetaNode.cs
@@ -1,21 +1,34 @@
+#region
 using System.Text;
 using Chaos.DarkAges.Definitions;
 using Chaos.IO.Memory;
 using Chaos.MetaData.Abstractions;
+#endregion
 
 namespace Chaos.MetaData.ItemMetaData;
 
 /// <summary>
 ///     A node that stores metadata about an item
 /// </summary>
-/// <param name="Name">
-/// </param>
-public sealed record ItemMetaNode(string Name) : IMetaNode
+public sealed record ItemMetaNode : IMetaNode
 {
     /// <summary>
     ///     The category of the item, used for bank sorting
     /// </summary>
-    public string Category { get; set; } = "other";
+    public string Category
+    {
+        get;
+
+        set
+        {
+            if (!string.IsNullOrEmpty(field))
+                Length -= Encoding.GetByteCount(field);
+
+            field = value;
+
+            Length += Encoding.GetByteCount(value);
+        }
+    }
 
     /// <summary>
     ///     The class that can equip the item
@@ -25,38 +38,96 @@ public sealed record ItemMetaNode(string Name) : IMetaNode
     /// <summary>
     ///     A short description of the item
     /// </summary>
-    public string Description { get; set; } = string.Empty;
+    public string Description
+    {
+        get;
 
-    /// <summary>
-    ///     The level required to equip the item
-    /// </summary>
-    public int Level { get; set; }
+        set
+        {
+            if (!string.IsNullOrEmpty(field))
+                Length -= Encoding.GetByteCount(field);
 
-    /// <summary>
-    ///     The name of the item
-    /// </summary>
-    public string Name { get; init; } = Name;
+            field = value;
 
-    /// <summary>
-    ///     The weight of the item
-    /// </summary>
-    public int Weight { get; set; }
+            Length += Encoding.GetByteCount(value);
+        }
+    }
 
     /// <summary>
     ///     The length of the serialized data
     /// </summary>
-    public int Length
-        => 14
-           + Encoding.GetEncoding(949)
-                     .GetByteCount(Name)
-           + Encoding.GetEncoding(949)
-                     .GetByteCount(Category)
-           + Encoding.GetEncoding(949)
-                     .GetByteCount(Description)
-           + Level.ToString()
-                  .Length
-           + Weight.ToString()
-                   .Length;
+    public int Length { get; private set; }
+
+    /// <summary>
+    ///     The level required to equip the item
+    /// </summary>
+    public int Level
+    {
+        get;
+
+        set
+        {
+            Length -= field.ToString()
+                           .Length;
+
+            field = value;
+
+            Length += field.ToString()
+                           .Length;
+        }
+    }
+
+    /// <summary>
+    ///     The name of the item
+    /// </summary>
+    public string Name
+    {
+        get;
+
+        init
+        {
+            if (!string.IsNullOrEmpty(field))
+                Length -= Encoding.GetByteCount(field);
+
+            field = value;
+
+            Length += Encoding.GetByteCount(value);
+        }
+    }
+
+    /// <summary>
+    ///     The weight of the item
+    /// </summary>
+    public int Weight
+    {
+        get;
+
+        set
+        {
+            Length -= field.ToString()
+                           .Length;
+
+            field = value;
+
+            Length += field.ToString()
+                           .Length;
+        }
+    }
+
+    private static Encoding Encoding { get; } = Encoding.GetEncoding(949);
+
+    /// <summary>
+    ///     A node that stores metadata about an item
+    /// </summary>
+    /// <param name="name">
+    /// </param>
+    public ItemMetaNode(string name)
+    {
+        Length = 16;
+        Category = "other";
+        Description = string.Empty;
+        Name = name;
+    }
 
     /// <inheritdoc />
     public void Serialize(ref SpanWriter writer)
@@ -70,4 +141,11 @@ public sealed record ItemMetaNode(string Name) : IMetaNode
         writer.WriteString16(Category); // 2 bytes for length + length of string
         writer.WriteString16(Description); // 2 bytes for length + length of string
     }
+
+    /// <summary>
+    ///     Deconstructs the <see cref="ItemMetaNode" /> into its components
+    /// </summary>
+    /// <param name="name">
+    /// </param>
+    public void Deconstruct(out string name) => name = Name;
 }
